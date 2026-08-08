41399d70138d6c93039e6f665bdd09ba9b2f5b64
Weston Harper <13283469+wesharper@users.noreply.github.com>
2026-08-08 10:31:55 -0600

    add brave origin shortcut

--- stow/.config/hypr/binds/applications.lua
+++ stow/.config/hypr/binds/applications.lua
@@ -2,3 +2,4 @@ local mainMod = "SUPER"
 
 hl.bind(mainMod .. "+ RETURN", hl.dsp.exec_cmd("uwsm app -T"))
 hl.bind(mainMod .. "+ SHIFT + backslash", hl.dsp.exec_cmd("uwsm app 1password"))
+hl.bind(mainMod .. "+ SHIFT + RETURN", hl.dsp.exec_cmd("uwsm app brave-origin"))
